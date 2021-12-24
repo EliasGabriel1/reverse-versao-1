2347d95bca9335e0682314cbe961c409e96136bd
Japa0404”git config --global user.name Japa0404” <anajuliataborda0402@gmail.com>
2021-12-23 22:50:47 -0300

    update

---
+++ content/js/index.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function () {
+    $('.lazy').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
